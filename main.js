94e0432aeaf535b05bc35c5f5b03225ca73f9e95
Klay Thacker <klaythacker11@gmail.com>
2021-07-20 20:55:24 -0500

    user id added and tested in browser

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("It's Great");
+let group6 = [
+{
+    id: 1,
+    name: "Nissa",
+    favoriteColor: "pink",
+    somethingUnique: "I like stilletos",
+    userImg: "https://avatars.githubusercontent.com/u/86807857?v=4",
+},
+{
+    id: 2,
+    name: "Daniel",
+    favoriteColor: "teal",
+    somethingUnique: "I build PCs",
+    userImg: "https://avatars.githubusercontent.com/u/82732748?v=4",
+},
+{
+    id: 3,
+    name: "Halie",
+    email: "halie@email.com",
+    favoriteColor: "green",
+    somethingUnique: "allergic to advil",
+    userImg: "https://avatars.githubusercontent.com/u/86806913?v=4",
+},
+{
+    id: 4,
+    name: "Klay",
+    email: "klaythacker@email.com",
+    favoriteColor: "purple",
+    somethingUnique: "has hearing loss",
+    userImg: "https://avatars.githubusercontent.com/u/86626953?v=4",
+},
+];
+console.log(group6)
